b4081819cdab9cf07c6a978260028d644badc70a
Mohammed Rashid <rashidfaisu@gmail.com>
2020-06-23 11:12:56 +0530

    hai

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using test.Models;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace test.Controllers
+{
+    public class PersonController : Controller
+    {
+        private DataContext db = new DataContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string Search(string PersonName)
+        {            
+            var Person = new List<PersonViewModel>();
+            Person = db.Person.Where(p => p.PersonName.ToLower().Contains(PersonName.ToLower())).ToList();
+            Person.ForEach(a => a.label = a.PersonName);
+            return JsonConvert.SerializeObject(Person);
+        }
+
+        [HttpPost]
+        public Response SavePerson(PersonViewModel objPersonViewModel)
+        {
+            Response objResponse = new Response();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Person.Add(objPersonViewModel);
+                    db.SaveChanges();
+                    objResponse.title = "Success";
+                    objResponse.message = "Insert Success";
+                }
+                else
+                {
+                    objResponse.title = "Failed";
+                    objResponse.message = "Insert Failed. Check The fields are correct";
+                }
+            }
+            catch (Exception ex)
+            {
+                objResponse.title = "Failed";
+                objResponse.message = ex.Message;
+            }
+            return objResponse;
+        }
+
+        [HttpPost]
+        public IRestResponse SendEmail(string ToEmail,string Name,string Address)
+        {
+            RestClient client = new RestClient();
+            client.BaseUrl = new Uri("https://api.mailgun.net/v3");
+            client.Authenticator =
+                new HttpBasicAuthenticator("api",
+                                            "6d90282cd737563851034175d022eb8d-1b6eb03d-7b186e51");
+            RestRequest request = new RestRequest();
+            request.AddParameter("domain", "https://localhost:44393/", ParameterType.UrlSegment);
+            request.Resource = "{domain}/messages";
+            request.AddParameter("from", "Mohammed Rashid <rashidfaisu@gmail.com>");
+            request.AddParameter("to", "mohammedrashidmadathil@gmail.com");
+            request.AddParameter("to", "mohammedrashidmadathil@gmail.com");
+            request.AddParameter("subject", "User Details");
+            request.AddParameter("text", "Name:"+Name+"    Address:"+Address+"");
+            request.Method = Method.POST;
+            var data=client.Execute(request);
+            return data;
+        }
+    }
+}
---
+++ Models/PersonViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace test.Models
+{
+    [Table("Person")]
+    public class PersonViewModel
+    {
+        [Key]
+        [Column("PersonID")]
+        public int PersonID { get; set; }
+        [Column("PersonName")]
+        public string PersonName { get; set; }
+        [Column("PersonAddress")]
+        public string PersonAddress { get; set; }
+        [NotMapped]
+        public string label { get; set; }
+    }
+
+    public class Response
+    {
+        public string type { get; set; }
+        public string title { get; set; }
+        public string message { get; set; }
+        
+    }
+
+}
